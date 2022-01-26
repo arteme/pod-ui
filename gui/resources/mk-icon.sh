cc835ea6c0bd3b03a1182fa20113368d0b98477c
Artem Egorkine <arteme@gmail.com>
2022-01-26 13:02:03 +0200

    Fix icon generation for Windows

--- gui/resources/mk-icon.sh
+++ gui/resources/mk-icon.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-convert icon.png -resize 256x256 icon-256.png
-convert icon-256.png -resize 16x16 icon-16.png
-convert icon-256.png -resize 32x32 icon-32.png
-convert icon-256.png -resize 64x64 icon-64.png
-convert icon-256.png -resize 128x128 icon-128.png
-convert icon-16.png icon-32.png icon-64.png icon-128.png icon-256.png -colors 256 icon.ico
+convert icon.png -define icon:auto-resize=16,32,48,64,128,256 icon.ico
 
 
