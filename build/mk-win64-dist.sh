650ad1d428f2bab4323c8485f642e7a7058d9561
Artem Egorkine <arteme@gmail.com>
2022-09-27 16:58:34 +0300

    build/win64: allow for extra label + different target dir

--- build/mk-win64-dist.sh
+++ build/mk-win64-dist.sh
@@ -1,23 +1,26 @@
 # https://gist.github.com/mjakeman/0add69647a048a5e262d591072c7facb
 # maybe also add win10 theme? https://www.gtk.org/docs/installations/windows
 
+EXTRA=$1
+T=${2:-target}
+
 V=$(git describe --tags --always --match 'v*' --dirty)
-N=pod-ui-$V-win64
+N=pod-ui-$V-win64$EXTRA
 DIST=debug
-DIR=target/$N
+DIR=$T/$N
 TOOLS_DIR=$(dirname $0)
 
 rm -rf $DIR
-rm -rf "target/$N.zip"
+rm -rf "$T/$N.zip"
 
 mkdir -p $DIR
 
 $TOOLS_DIR/collect-gtk.sh $DIR
 
-cp target/$DIST/*.exe $DIR
+cp $T/$DIST/*.exe $DIR
 
 echo "Copying dlls..."
-ldd target/$DIST/*.exe | grep '/mingw.*/.*\.dll' -o | xargs -I{} cp '{}' $DIR
+ldd $T/$DIST/*.exe | grep '/mingw.*/.*\.dll' -o | xargs -I{} cp '{}' $DIR
 
 LOADERS=$(find $DIR/lib/gdk-pixbuf-2.0 -name '*.dll')
 ldd $LOADERS | grep '/mingw.*/.*\.dll' -o | xargs -I{} cp '{}' $DIR
