arteme/pod-ui

commit 34b549befccf0f3ac62b67339e701fa94c383ddd

POD 2.0 family: Fix possible crashes after an out-of-range CC 19 "effect_select:raw"
There is are valirous triggers (namely, "delay" and "effect_tweak") defined
in `mod-pod2/src/wiring.rs` that depend on a correct value of "effect_select:raw"
and assumed `effect_entry_for_value()` always returned a valid entry.
This may have been try once, but now it is an `Option<(...)>`. This leads to
crashes if effect select control sent an out-of-range value. And either the
delay is toggled in the UI or an effect tweak control sends a message.
This is likely the cause of #39.

Fixes #39

mod-pod2/src/wiring.rs
@@ -244,8 +244,8 @@ pub fn wire_effect_select(config: &Config, controller: Arc<Mutex<Controller>>, c
         .run(move |value, controller, origin, config| {
             if value != 0 {
                 let effect_select = controller.get("effect_select:raw").unwrap();
-                let (_, delay, idx) =
-                    effect_entry_for_value(&config, effect_select).unwrap();
+                let Some((_, delay, idx)) =
+                    effect_entry_for_value(&config, effect_select) else { return };
                 if !delay {
                     // if `delay_enable` was switched on in the UI and if coming from
                     // an effect which didn't have delay to begin with, check if it can
@@ -263,12 +263,9 @@ pub fn wire_effect_select(config: &Config, controller: Arc<Mutex<Controller>>, c
         .on("effect_tweak").from(MIDI).from(NONE)
         .run(move |value, controller, origin, config| {
             let effect_select = controller.get("effect_select:raw").unwrap();
-            let (entry, _, _) =
-                effect_entry_for_value(&config, effect_select).unwrap();
+            let Some((entry, _, _)) =
+                effect_entry_for_value(&config, effect_select) else { return };
             let control_name = &entry.effect_tweak;
-            if control_name.is_empty() {
-                return;
-            }
 
             // HACK: as if everything's coming straight from MIDI
             let config = controller.get_config("effect_tweak").unwrap();

commit 456aabb01bffd23e5108003139f278a94524ca68

POD 2.0 family: Fix more crashes after CC 19
34b549befccf0f3ac62b67339e701fa94c383ddd broke the case when
"effect_tweak" is an empty string (no effect tweak). Fix it.

mod-pod2/src/wiring.rs
@@ -266,6 +266,7 @@ pub fn wire_effect_select(config: &Config, controller: Arc<Mutex<Controller>>, c
             let Some((entry, _, _)) =
                 effect_entry_for_value(&config, effect_select) else { return };
             let control_name = &entry.effect_tweak;
+            if control_name.is_empty() { return }
 
             // HACK: as if everything's coming straight from MIDI
             let config = controller.get_config("effect_tweak").unwrap();